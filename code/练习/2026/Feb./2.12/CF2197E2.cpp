ceffb835e6ec8df7c3a1ab39156238c269b1d294
yqfff-qwq <190549306@qq.com>
2026-02-12 16:10:08 +0800

    2.12

---
+++ code/练习/2026/Feb./2.12/CF2197E2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <algorithm>
+#include <random>
+#include <map>
+
+// #define int long long
+#define REP(i,l,r) for(int i=l;i<=r;i++)
+#define DEP(i,r,l) for(int i=r;i>=l;i--)
+#define MAX(a, b) (a) = max((a), (b))
+#define MIN(a, b) (a) = min((a), (b))
+#define pii std::pair<int, int>
+#define fi first
+#define se second
+#define pb push_back
+#define ull unsigned long long
+#define ll long long
+
+void read(){}
+template<typename T1,typename ...T2>inline void read(T1 &x,T2 &...oth)
+{
+  x=0;
+  int f=0,ch=getchar();
+  while(ch<'0'||ch>'9')
+  {
+    if(ch=='-')
+    {
+      f=1;
+    }
+    ch=getchar();
+  }
+  while('0'<=ch&&ch<='9')
+  {
+    x=(x<<3)+(x<<1)+(ch^48);
+    ch=getchar();
+  }
+  if(f)
+  {
+    x=-x;
+  }
+  read(oth...);
+}
+
+namespace YZLK{
+  const int N = 40;
+  std::vector<int> ve[N];
+  int n, m;
+  void guess() {
+    std::cout << "! " << m << '\n';
+    REP(i, 1, n)
+    return;
+  }
+  void main() {
+    read(n);
+    REP(i, 1, n)  ve[i].clear();
+    
+    return ;
+  }
+}
+
+signed main()
+{
+  // freopen("array.in","r",stdin);
+  // freopen("array.out","w",stdout);
+  int T=1;
+  // read(T);
+  while(T--)
+  {
+    YZLK::main();
+  }
+
+  fclose(stdin);
+  fclose(stdout);
+  return 0;
+}
+/*
+
+code by yqfff_qwq
+
+交代码之前看一下
+
+这是你的代码吗喵喵？这是你要交的题吗喵喵？
+
+多测了吗喵喵？多测清空了吗喵喵？多测清空会超时吗喵喵？会出现其他问题吗喵喵？
+
+数组开小了吗喵喵？模数正确吗喵喵？调试删干净了吗喵喵？
+
+*/
